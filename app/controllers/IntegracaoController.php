5c0631a04b30ea7a7e6f792dc37838eaff675c6c
Gabriel Victor S. Quintino <55193440+Gabrielquintino@users.noreply.github.com>
2024-03-19 17:57:16 -0300

    conexao aws

--- app/controllers/IntegracaoController.php
+++ app/controllers/IntegracaoController.php
@@ -4,8 +4,9 @@ namespace App\Controllers;
 
 use App\Models\UsuarioModel;
 use Aws\S3\S3Client;
-use Aws\S3\Exception\S3Exception;
+use Aws\Exception\AwsException;
 use stdClass;
+use DateTime;
 
 class IntegracaoController
 {
@@ -22,27 +23,51 @@ class IntegracaoController
 
     public function listagem()
     {
+        // Configurações da AWS
+        $bucketName = 'wwebjs'; // Substitua pelo nome do seu bucket
+        $prefix = 'sessions'; // Substitua pelo prefixo da pasta que deseja listar
 
-        $folderPath = 'C:/code/GestaoFacil/resources/js/.wwebjs_auth';
-        $prefix = 'session-';
+        // Configuração do cliente S3
+        $s3Client = new S3Client([
+            'region'  => 'us-east-1', // Substitua pela região da sua S3
+            'credentials' => [
+                'key'    => AWS_ACCESS_KEY,
+                'secret' => AWS_SECRET_ACCESS_KEY,
+            ],
+            'http' => [
+                'verify' => false, // Desativar a verificação SSL
+            ],
+        ]);
 
-        $folders = array_filter(scandir($folderPath), function ($item) use ($prefix) {
-            return is_dir('C:/code/GestaoFacil/resources/js/.wwebjs_auth' . '/' . $item) && strpos($item, $prefix) === 0;
-        });
+        try {
+            // Lista os objetos na pasta
+            $objects = $s3Client->listObjects([
+                'Bucket' => $bucketName,
+                'Prefix' => $prefix
+            ]);
 
-        $arrLista = [
-            'data' => [],
-            'success' => false
-        ];
+            $arrLista = [
+                'data' => []
+            ];
 
-
-        foreach ($folders as $folder) {
-            $folderPath .= '/' . $folder;
-            $objItem = new stdClass();
-            $objItem->nome = str_replace($prefix, '', $folder);
-
-            array_push($arrLista['data'], $objItem);
+            // Itera sobre os objetos e exibe o nome e a data de modificação de cada um
+            foreach ($objects['Contents'] as $object) {
+                if ($object['Key'] !== "sessions/") {
+                    $objItem = new stdClass;
+                    preg_match('/(\d+)/',$object['Key'], $matches);
+                    $objItem->nome = $matches[0];
+                    $data = new DateTime($object['LastModified']);
+                    $objItem->data = $data->format('d/m/Y H:i:s');
+                    array_push($arrLista['data'], $objItem);
+                }
+            }
+        } catch (AwsException $e) {
+            // Captura e exibe erros da AWS
+            echo $e->getMessage() . "\n";
         }
+
+
+
 
         $arrLista['success'] = true;
 
--- resources/js/wweb.js
+++ resources/js/wweb.js
@@ -3,7 +3,38 @@ const cors = require('cors');
 const qrcode = require('qrcode-terminal');
 const app = express();
 const port = 3000;
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, RemoteAuth } = require('whatsapp-web.js');
+const { AwsS3Store } = require('wwebjs-aws-s3');
+const {
+  S3Client,
+  PutObjectCommand,
+  HeadObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand
+} = require('@aws-sdk/client-s3');
+
+const s3 = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: 'AKIA47CR26CB3JJ3Y3WM',
+    secretAccessKey: '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'
+  }
+});
+
+const putObjectCommand = PutObjectCommand;
+const headObjectCommand = HeadObjectCommand;
+const getObjectCommand = GetObjectCommand;
+const deleteObjectCommand = DeleteObjectCommand;
+
+const store = new AwsS3Store({
+  bucketName: 'wwebjs',
+  remoteDataPath: 'sessions/',
+  s3Client: s3,
+  putObjectCommand,
+  headObjectCommand,
+  getObjectCommand,
+  deleteObjectCommand
+});
 
 // Adicione o middleware cors
 app.use(cors());
@@ -13,37 +44,43 @@ app.get('/qrcode', (req, res) => {
 
   const { clientId } = req.query;
 
-  const client1 = new Client({
-    authStrategy: new LocalAuth({ clientId: clientId })
+  const client = new Client({
+    authStrategy: new RemoteAuth({
+      clientId: clientId,
+      dataPath: 'sessions',
+      store: store,
+      backupSyncIntervalMs: 600000
+    })
   });
 
-  client1.initialize();
+  client.initialize();
 
-  client1.on('message', async (message) => {
+  client.on('message', async (message) => {
     if (message.body === 'oi') {
       await message.reply('oi tudo bem? como posso ajudar você hoje?');
     }
   });
 
-  client1.on('qr', (qr) => {
+  client.on('qr', (qr) => {
     res.write(JSON.stringify({ type: 'qr', data: qr, clientId: clientId }));
     res.end(); // Finaliza a resposta após enviar o QR code
     console.log('QR code gerado para ' + clientId);
   });
 
-
-
-});
-
-app.get('/conectado', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  Client.on('ready', () => {
-    res.write(JSON.stringify({ type: 'ready' }));
-    res.end(); // Finaliza a resposta após enviar a prontidão
-    console.log('Client is ready!');
+  app.get('/conectado', async (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    client.on('ready', () => {
+      console.log('Client is ready!');
+      res.write(JSON.stringify({ type: 'ready' }))
+      res.end();
+    });
+  
   });
 });
 
+// Definir a rota '/conectado'
+
+
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://3.81.81.196:${port}`);
 });
